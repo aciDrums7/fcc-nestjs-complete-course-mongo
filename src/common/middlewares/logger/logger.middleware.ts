9c72686a0f3818f536e0524c21ca4f8eb72bd786
aciDrums7 <edoardo.carradori.96@gmail.com>
2024-05-06 10:13:45 +0000

    Fixed logger

--- src/common/middlewares/logger/logger.middleware.ts
+++ src/common/middlewares/logger/logger.middleware.ts
@@ -2,7 +2,7 @@ import { Injectable, Logger, NestMiddleware, Req } from '@nestjs/common';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  use(@Req() req: any, next: () => void) {
+  use(@Req() req: any, res: any, next: () => void) {
     const now = new Date();
     Logger.log(
       `${now.toDateString()}, ${now.toLocaleTimeString()} - Request ${req.method} ${req.baseUrl}`
